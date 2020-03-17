126d781fff7f956df25429bb7213e02badaed291
carlos melo <carlaofernandesedu@gmail.com>
2020-03-17 00:02:31 -0300

    adicionado projeto configserverUI

---
+++ autorizacao/IdentityServerUI/Config.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4.Test;
+using IdentityServer4;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] { new ApiResource("api1", "My API") };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "usuario1",
+                    Password = "123456"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "usuario2",
+                    Password = "123456"
+                }
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] { 
+                new Client{
+                    ClientId="client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+
+                    },
+                    AllowedScopes = {"api1"}
+                },
+                new Client{
+                    ClientId="clientpass",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+
+                    },
+                    AllowedScopes = {"api1"}
+                },
+                new Client
+                {
+                    ClientId = "mvcnetcore",
+                    ClientName = "MVC Net Core Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+
+                    // where to redirect to after login
+                    RedirectUris = { "https://localhost:5004/signin-oidc" },
+
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "https://localhost:5004/signout-callback-oidc" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                }
+            };
+        }
+    }
+}
